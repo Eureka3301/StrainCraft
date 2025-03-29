3aa83ba0fbaa8f2cc322ccff35af6ee495cdfb3f
Rodion Lukashov <32921341+Eureka3301@users.noreply.github.com>
2025-03-30 01:51:00 +0300

    First upload of the main program files.

---
+++ main.py
@@ -0,0 +1,18 @@
+from mySHPB_lib import SHPB_test
+
+test_props = {
+    "filename": 'exp\Mg_11.03\RigolDS0.csv',
+    "setupPropsFile": 'props2025march.json',
+    "Ls/mm": 4.0,
+    "Ds/mm": 7.8
+}
+
+exp = SHPB_test(**test_props)
+
+import matplotlib.pyplot as plt
+
+exp.plot_diagrams()
+plt.show()
+
+exp.plot_diagram()
+plt.show()
